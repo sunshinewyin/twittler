926dae4c101a640b1467541a4b8a7edfd3524f85
Sunshine Yin <sunshine.w.yin@gmail.com>
2015-02-20 19:06:43 -0800

    Blue and orange theme

--- twittler.js
+++ twittler.js
@@ -2,21 +2,26 @@ $(document).ready(function(){
   	var $body = $('body');
   	var $tweetsBody = $('.tweetsBody');
   	var $header = $("#header");
-  	var $userTweets = $('.userTweets')
+
+  	var $shawndrosttimeline = $('#shawndrosttimeline');
+  	var $sharksforcheaptimeline = $('#sharksforcheaptimeline');
+  	var $mracustimeline = $('#mracustimeline');
+  	var $douglascalhountimeline = $('#douglascalhountimeline');
 
 // Set up streams to appear on "refresh" button
-    $("#header button").on("click", function(){
+    $("#refreshtweetsbutton").on("click", function(){
       	var index = streams.home.length - 1;
       	$tweetsBody.empty();
 
 	    while(index >= 0){
 	        var tweet = streams.home[index];
-	        var $tweet = $('<div class="tweetSection"</div>');
+	        var $tweet = $('<div id="tweetSection" class="allTweets"></div>');
 	        var timeStamp = new Date();
-	        var $timeStamp = $('<p class="tweetSection"></p>')
+	        var $timeStamp = $('<div id="timeStamp" class="allTweets"></div>');
 	        $tweet.text('@' + tweet.user + ': ' + tweet.message);
 	        $timeStamp.text(timeStamp);
-	      	$tweetsBody.append($tweet,$timeStamp);
+	        $tweet.append($timeStamp);
+	      	$tweetsBody.append($tweet);
 	        index -= 1;
       	};
       });
@@ -24,20 +29,63 @@ $(document).ready(function(){
 //display individual users' tweets
 	$("#shawndrosttweets").on("click", function(){
       	var index = streams.users.shawndrost.length - 1;
-      	$userTweets.empty();
+      	$shawndrosttimeline.empty();
 	    while(index >= 0){
-	      	var shawndrosttweet = streams.users.shawndrost[index].message;
-	      	var $shawndrosttimeline = $('<div id = "shawndrosttimeline"></div>');
+	      	var shawndrosttweets = streams.users.shawndrost[index].message;
+	      	var $shawndrosttweets = $('<div id = "shawndrosttimeline"></div>');
 	        var timeCreated = streams.users.shawndrost[index].created_at;
 	        var $timeCreated = $('<p id="shawndrosttimeline"></p>')
-	        $shawndrosttimeline.text(shawndrosttweet);
+	        $shawndrosttimeline.text(shawndrosttweets);
 	        $timeCreated.text(timeCreated);
-	      	$userTweets.append($shawndrosttimeline, $timeCreated);
+	      	$shawndrosttimeline.append($shawndrosttweets, $timeCreated);
 	        index -= 1;
       	};
-
-
       });
+
+	$("#sharksforcheaptweets").on("click", function(){
+      	var index = streams.users.sharksforcheap.length - 1;
+      	$sharksforcheaptimeline.empty();
+	    while(index >= 0){
+	      	var sharksforcheaptweets = streams.users.sharksforcheap[index].message;
+	      	var $sharksforcheaptweets = $('<div id = "sharksforcheaptimeline"></div>');
+	        var timeCreated = streams.users.sharksforcheap[index].created_at;
+	        var $timeCreated = $('<p id="sharksforcheaptimeline"></p>')
+	        $sharksforcheaptweets.text(sharksforcheaptweets);
+	        $timeCreated.text(timeCreated);
+	      	$sharksforcheaptimeline.append($sharksforcheaptweets, $timeCreated);
+	        index -= 1;
+      	};
+      });
+
+	// $("#shawndrosttweets").on("click", function(){
+ //      	var index = streams.users.shawndrost.length - 1;
+ //      	$userTweets.empty();
+	//     while(index >= 0){
+	//       	var shawndrosttweet = streams.users.shawndrost[index].message;
+	//       	var $shawndrosttimeline = $('<div id = "shawndrosttimeline"></div>');
+	//         var timeCreated = streams.users.shawndrost[index].created_at;
+	//         var $timeCreated = $('<p id="shawndrosttimeline"></p>')
+	//         $shawndrosttimeline.text(shawndrosttweet);
+	//         $timeCreated.text(timeCreated);
+	//       	$userTweets.append($shawndrosttimeline, $timeCreated);
+	//         index -= 1;
+ //      	};
+ //      });
+
+	// $("#shawndrosttweets").on("click", function(){
+ //      	var index = streams.users.shawndrost.length - 1;
+ //      	$userTweets.empty();
+	//     while(index >= 0){
+	//       	var shawndrosttweet = streams.users.shawndrost[index].message;
+	//       	var $shawndrosttimeline = $('<div id = "shawndrosttimeline"></div>');
+	//         var timeCreated = streams.users.shawndrost[index].created_at;
+	//         var $timeCreated = $('<p id="shawndrosttimeline"></p>')
+	//         $shawndrosttimeline.text(shawndrosttweet);
+	//         $timeCreated.text(timeCreated);
+	//       	$userTweets.append($shawndrosttimeline, $timeCreated);
+	//         index -= 1;
+ //      	};
+ //      });
 
 	// var $sharksforcheapTimeline = $('<div id="sharksforcheapTimeline"</div>');
  //  	var $mracusTimeline = $('<div id="mracusTimeline"</div>');
